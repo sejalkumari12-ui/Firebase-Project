21e62b6f9a6c7337a87d88104e179b3094c77eb5
sejal <user.email: sejalkumari5679@gmail.com>
2025-10-10 13:59:06 +0530

    router done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Dashboard from "./Components/Dashboard"
+import AddStudent from "./Components/AddStudent";
+import StudentList from "./Components/StudentList";
+
+
+
+const myRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />, 
+    children:[
+      {path:"",element:<StudentList/>},
+      {path:"addStudent",element:<AddStudent />},
+      {path:"studentList" , element:<StudentList />}
+    ]
+  },
+]);
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <>
+    <RouterProvider router={myRouter} />
+    </>
   )
 }
 
